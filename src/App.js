dab9bf490e5c55bc3fa4ff00c8c2a820d20f8f64
SaraAzmin <azmin.sara17@gmail.com>
2022-05-26 16:17:27 +0200

    router and pages created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
+import Home from './Pages/Home/Home';
+import Blogs from './Pages/Blogs/Blogs';
+import Navbar from "./Pages/Shared/Navbar";
 
 function App() {
   return (
     <div>
-
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>} />
+        <Route path="/home" element={<Home></Home>} />
+        <Route path="blogs" element={<Blogs></Blogs>} />
+      </Routes>
     </div>
   );
 }
