f811218ff23bde35786ff83565fe097144cf5373
SaraAzmin <azmin.sara17@gmail.com>
2022-05-26 17:04:42 +0200

    responsive banner added

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -12,13 +12,13 @@ const Navbar = () => {
 
     return (
         <div>
-            <div className="navbar bg-zinc-700 text-stone-100">
+            <div className="navbar bg-rose-700 text-stone-100">
                 <div className="navbar">
                     <div className="dropdown">
-                        <label tabIndex="0" className="btn btn-ghost lg:hidden">
+                        <label tabIndex="0" className="btn btn-black lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-stone-300-100 text-black rounded-box w-52">
+                        <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-stone-100 text-black rounded-box w-52">
                             {menuitems}
                         </ul>
                     </div>
