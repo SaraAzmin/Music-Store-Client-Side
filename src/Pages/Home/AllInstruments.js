f7e928df02176fe504f0a77e7d2aa1d4138d3d47
SaraAzmin <azmin.sara17@gmail.com>
2022-05-27 01:50:57 +0200

    info loaded for purchase page

--- src/Pages/Home/AllInstruments.js
+++ src/Pages/Home/AllInstruments.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const AllInstruments = () => {
 
@@ -11,8 +12,35 @@ const AllInstruments = () => {
     }, []);
 
     return (
-        <div>
-            <h1>{instruments.length}</h1>
+        <div className='px-10 lg:px-20 py-10'>
+            <h3 className='text-2xl lg:text-4xl pb-5 lg:pb-10 text-center font-semibold text-rose-700 uppercase'>Our Instruments</h3>
+            <div className="overflow-x-auto">
+                <table className="table table-zebra w-full">
+                    <thead>
+                        <tr className='text-center'>
+                            <th>SL. No</th>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Available Quantity</th>
+                            <th>Minimum Order</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody className='text-center'>
+                        {
+                            instruments.map((instrument, index) => <tr>
+                                <th>{index + 1}</th>
+                                <td>{instrument.name}</td>
+                                <td>$ {instrument.price}</td>
+                                <td>{instrument.availableQuantity}</td>
+                                <td>{instrument.minQuantity}</td>
+                                <td>
+                                    <Link to={`/instruments/${instrument._id}`}><button className='btn btn-xs bg-rose-700'>PURCHASE</button></Link></td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div >
         </div >
     );
 };
--- src/Pages/Home/Purchase.js
+++ src/Pages/Home/Purchase.js
@@ -1,45 +1,67 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Purchase = () => {
 
     const { id } = useParams();
+    const [instrument, setInstrument] = useState({});
+    const [user] = useAuthState(auth);
+
+    useEffect(() => {
+        const url = `http://localhost:5000/instruments/${id}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setInstrument(data))
+    }, []);
 
     return (
         <div className='px-10 lg:px-20 py-8 lg:py-14 flex flex-col bg-gray-100'>
             <h3 className='text-2xl lg:text-3xl pb-5 lg:pb-10 text-center font-semibold text-rose-700 uppercase'>Purchase Instrument</h3>
-            <div className='md:flex justify-center items-center mb-5 border-2 lg:w-1/2 mx-auto p-5 rounded-md border-green-500'>
+            <div className='md:flex justify-center items-center mb-5 border-2 lg:w-2/3 mx-auto p-10 rounded-md border-rose-700 bg-white'>
                 <div className='md:mr-5 lg:w-2/5 mb-5 md:mb-0'>
-                    <img className='h-[12rem] mx-auto' alt="" />
+                    <img className='h-[12rem] mx-auto' src={instrument.img} alt="" />
                 </div>
                 <div className='lg:w-3/5 justify-start items-start'>
                     <div className="flex mb-5">
-                        <h5 className='text-gray-900 text-lg font-medium pr-3'>Name: </h5>
-                        <h5 className='text-gray-900 text-lg font-normal'></h5>
-                    </div>
-                    <div className="flex  mb-5">
-                        <h5 className='text-gray-900 text-lg font-medium pr-3'>Supplier: </h5>
-                        <h5 className='text-gray-900 text-lg font-normal'></h5>
+                        <h5 className='text-gray-900 text-lg font-medium pr-3'>Instrument Name: </h5>
+                        <h5 className='text-gray-900 text-lg font-normal'>{instrument.name}</h5>
                     </div>
                     <div className="flex mb-5">
                         <h5 className='text-gray-900 text-lg font-medium pr-3'>Price: </h5>
-                        <h5 className='text-gray-900 text-lg font-normal'>$</h5>
+                        <h5 className='text-gray-900 text-lg font-normal'>${instrument.price}</h5>
                     </div>
                     <div className="flex mb-5">
-                        <h5 className='text-gray-900 text-lg font-medium pr-3'>Quantity: </h5>
-                        <h5 className='text-gray-900 text-lg font-normal' name='quantity'></h5>
-                        <button type="button" className="inline-block px-5 py-2 bg-green-600 text-white font-medium text-sm leading-tight uppercase rounded shadow-md hover:bg-green-800 hover:shadow-lg ml-3">Deliver</button>
+                        <h5 className='text-gray-900 text-lg font-medium pr-3'>Available Quantity: </h5>
+                        <h5 className='text-gray-900 text-lg font-normal' name='quantity'>{instrument.availableQuantity}</h5>
                     </div>
-                    <div className="md:flex mb-5">
-                        <div>
-                            <h5 className='text-gray-900 text-lg font-medium pr-3'>Add Stock: </h5>
-                            <input
-                                type="number"
-                                className="form-control px-2 py-1 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                                placeholder="stock input" name='valueAdd' />
-                        </div>
-                        <button type="button" className=" inline-block px-5 mt-3 md:mt-0 py-2 bg-green-600 text-white font-medium text-sm leading-tight uppercase rounded shadow-md hover:bg-green-800 hover:shadow-lg ml-3">Update</button>
+                    <div className="md:flex items-center mb-5">
+                        <h5 className='text-gray-900 text-lg font-medium pr-3'>Order Quantity: </h5>
+                        <input
+                            type="number"
+                            className="input input-bordered w-full max-w-xs" name='orderInput' value={instrument.minQuantity} />
                     </div>
+                    <div className="flex mb-5 items-center">
+                        <h5 className='text-gray-900 text-lg font-medium pr-3'>Customer Name: </h5>
+                        <input type="text" name="userName" disabled value={user?.displayName} className="input input-bordered w-full max-w-xs" />
+                    </div>
+                    <div className="flex mb-5 items-center">
+                        <h5 className='text-gray-900 text-lg font-medium pr-3'>Customer Email: </h5>
+                        <input type="text" name="userEmail" disabled value={user?.email} className="input input-bordered w-full max-w-xs" />
+                    </div>
+                    <div className="flex mb-5 items-center">
+                        <h5 className='text-gray-900 text-lg font-medium pr-3'>Customer Address: </h5>
+                        <input type="text" name="userAddress" className="input input-bordered w-full max-w-xs" />
+                    </div>
+                    <div className="flex mb-5 items-center">
+                        <h5 className='text-gray-900 text-lg font-medium pr-3'>Customer Phone: </h5>
+                        <input type="text" name="userPhone" className="input input-bordered w-full max-w-xs" />
+                    </div>
+                    <div className="flex mb-5 items-center">
+                        <Link to="/" type="button" className="mx-auto px-6 py-4 mt-7 bg-rose-700 text-white font-semibold text-lg rounded shadow-md">BUY NOW</Link>
+                    </div>
+
                 </div>
             </div>
         </div>
