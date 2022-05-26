5db5d63665019739fba88b8f7fbbc781284802b3
SaraAzmin <azmin.sara17@gmail.com>
2022-05-26 17:30:57 +0200

    responsive footer added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from './Banner';
+import Footer from '../Shared/Footer';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Footer></Footer>
         </div>
     );
 };
