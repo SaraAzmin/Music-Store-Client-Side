38f2d02f22a866aab43f44bb7dc70235b8c2f298
SaraAzmin <azmin.sara17@gmail.com>
2022-05-27 14:41:00 +0200

    all orders view added in my orders

--- src/Pages/DashBoard/Dashboard.js
+++ src/Pages/DashBoard/Dashboard.js
@@ -1,19 +1,24 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, Outlet } from 'react-router-dom';
+import auth from '../../firebase.init';
 
 const Dashboard = () => {
+
+    const [user] = useAuthState(auth);
     return (
         <div className="drawer drawer-mobile">
             <input id="dashboard-sidebar" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content">
-                <h2 className='text-3xl font-semibold text-rose-700 text-center pt-10 uppercase'>Welcome to your Dashboard</h2>
+                <h2 className='text-2xl font-semibold  text-center pt-5 uppercase'>Hello <span className='text-rose-700'>{user.displayName}</span></h2>
+                <h2 className='text-xl font-semibold text-center pt-2 pb-5 uppercase border-b-2 border-red-100'>Welcome to your Dashboard</h2>
                 <Outlet></Outlet>
             </div>
             <div className="drawer-side bg-gray-100 px-10">
                 <label htmlFor="dashboard-sidebar" className="drawer-overlay"></label>
                 <ul className="menu p-4 overflow-y-auto w-48 text-black font-medium">
                     {/* <!-- Sidebar content here --> */}
-                    <li className='border-b-2'><Link to="/dashboard">My Order</Link></li>
+                    <li className='border-b-2'><Link to="/dashboard">My Orders</Link></li>
                     <li className='border-b-2'><Link to="/dashboard/addReview">Add a Review</Link></li>
                     <li className='border-b-2'><Link to="/dashboard/myProfile">My Profile</Link></li>
                     {/* {
--- src/Pages/DashBoard/MyOrders.js
+++ src/Pages/DashBoard/MyOrders.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 
 const MyOrders = () => {
+
+    const [orders, setOrders] = useState([]);
+    const [user] = useAuthState(auth);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/order?customerEmail=${user.email}`)
+            .then(res => res.json())
+            .then(data => setOrders(data))
+
+    }, []);
+
     return (
         <div>
-
+            <div className="overflow-x-auto p-10">
+                <table className="table table-zebra w-full">
+                    <thead>
+                        <tr className='text-center'>
+                            <th>SL. No</th>
+                            <th>Instrument Name</th>
+                            <th>Price</th>
+                            <th>Order Quantity</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody className='text-center'>
+                        {
+                            orders.map((order, index) => <tr>
+                                <th>{index + 1}</th>
+                                <td>{order.productName}</td>
+                                <td>$ {order.orderPrice}</td>
+                                <td>{order.orderQuantity}</td>
+                                <td>
+                                    <Link to='/'><button className='btn btn-xs bg-rose-700'>Cancel Order</button></Link></td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div >
         </div>
     );
 };
